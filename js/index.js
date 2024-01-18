3a4cb072ec563af47e6a74594b03cdd19381206c
augustopiatto <ampiatto@hotmail.com>
2024-01-18 20:24:25 -0300

    animacao de favoritar

--- js/index.js
+++ js/index.js
@@ -1,15 +1,18 @@
 const showDetails1 = document.getElementById("expand-1");
 const showDetails2 = document.getElementById("expand-2");
+const favoriteIcon = document.getElementById("favorite-icon");
 
 showDetails1?.addEventListener("click", () => expandCardDetails("expand-container-1"));
 showDetails2?.addEventListener("click", () => expandCardDetails("expand-container-2"));
+favoriteIcon?.addEventListener("click", () => expandCardDetails("favorite-icon"));
 
 function expandCardDetails(elementId) {
   const productDetails = document.getElementById(elementId)
 
-  if(productDetails.classList.contains("show")) {
-    productDetails.classList.remove("show")
+  if(productDetails.classList.contains("clicked")) {
+    productDetails.classList.remove("clicked")
   } else {
-    productDetails.classList.add("show")
+    productDetails.classList.add("clicked")
   }
 }
+
