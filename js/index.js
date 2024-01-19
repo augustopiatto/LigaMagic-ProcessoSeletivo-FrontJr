cff8f04078b024f703969dd81ddba0ca1dc7706c
augustopiatto <ampiatto@hotmail.com>
2024-01-18 22:55:09 -0300

    dropdown de mais infos

--- js/index.js
+++ js/index.js
@@ -1,18 +1,20 @@
 const showDetails1 = document.getElementById("expand-1");
 const showDetails2 = document.getElementById("expand-2");
 const favoriteIcon = document.getElementById("favorite-icon");
+const moreInfo = document.getElementById("more-info");
 
-showDetails1?.addEventListener("click", () => expandCardDetails("expand-container-1"));
-showDetails2?.addEventListener("click", () => expandCardDetails("expand-container-2"));
-favoriteIcon?.addEventListener("click", () => expandCardDetails("favorite-icon"));
+showDetails1?.addEventListener("click", () => addClickedClass("expand-container-1"));
+showDetails2?.addEventListener("click", () => addClickedClass("expand-container-2"));
+favoriteIcon?.addEventListener("click", () => addClickedClass("favorite-icon"));
+moreInfo?.addEventListener("click", () => addClickedClass("dropdown"));
 
-function expandCardDetails(elementId) {
-  const productDetails = document.getElementById(elementId)
+function addClickedClass(elementId) {
+  const element = document.getElementById(elementId)
 
-  if(productDetails.classList.contains("clicked")) {
-    productDetails.classList.remove("clicked")
+  if(element.classList.contains("clicked")) {
+    element.classList.remove("clicked")
   } else {
-    productDetails.classList.add("clicked")
+    element.classList.add("clicked")
   }
 }
 
